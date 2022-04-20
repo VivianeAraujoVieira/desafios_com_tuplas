d195ba9bcbf8957b54e22aea80949c73473fd1d4
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-04-20 15:40:41 -0300

    Update 4_desafio_usando_tuplas.py
    inclusão de valores pares

--- 4_desafio_usando_tuplas.py
+++ 4_desafio_usando_tuplas.py
@@ -8,15 +8,25 @@ b) Em que posição foi digitado o primeiro valor 3.
 
 c) Quais foram os números pares. """
 
+
+
 valores = (int(input("Informe o primeiro valor: ")),
            int(input("Informe o segundo valor: ")),
            int(input("informe o terceiro valor: ")),
            int(input("Informe o quarto valor: " )))
 
+
+
 print(f"Todos o valores são esses: {valores}")
 print(f"O valor 9 aparece {valores.count(9)} vezes")
-print(f"O valor 3 esta na {valores.index(3)}ª posição")
+if 3 in valores:
+    print(f"O valor 3 esta na {valores.index(3)}ª posição")
+else:
+    print("Não houve nenhum valor 3 digitado")
 
+for n in valores:
+    if n % 2 ==0:
+        print(f"Os valores que são  pares:  {n}")
 
 
 
